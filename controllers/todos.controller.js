9709843d40f77b24506c0ac576281a7f9b12734f
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-11-27 16:50:59 +0100

    handle todos

--- controllers/todos.controller.js
+++ controllers/todos.controller.js
@@ -14,7 +14,7 @@ async function getAllTodos(req, res, next) {
     })
 }
 
-async function addTodo(req, res, next) { 
+async function addTodo(req, res, next) {
     const todoText = req.body.text;
 
     const todo = new Todo(todoText);
@@ -23,19 +23,40 @@ async function addTodo(req, res, next) {
     try {
         const result = await todo.save();
         insertedId = result.insertedId;
-    } catch(error) {
+    } catch (error) {
         return next(error);
     }
 
     todo.id = insertedId.toString();
-    res.json({message: 'Added todo successfully!', createTodo: todo})
+    res.json({ message: 'Added todo successfully!', createTodo: todo })
 }
 
-function updateTodo(req, res, next) { 
+async function updateTodo(req, res, next) {
+    const todoId = req.params.id;
+    const newTodoText = req.body.newText;
+    const todo = new Todo(newTodoText, todoId);
+    try {
+        await todo.save()
+    } catch (error) {
+        return next(error);
+    };
 
+    res.json({ message: 'Todo updated!', updatedTodo : todo});
 }
 
-function deleteTodo(req, res, next) { }
+async function deleteTodo(req, res, next) {
+    const todoId = req.params.id;
+    const todo = new Todo(null, todoId);
+
+    try {
+        await todo.delete()
+    } catch (error) {
+        return next(error);
+    };
+
+    res.json({ message: 'Todo deleted!'});
+
+}
 
 
 module.exports = {
